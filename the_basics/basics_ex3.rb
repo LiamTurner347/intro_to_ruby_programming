b9e3c10b6712c2bed383e70a2f1fe3a18aa2347a
Liam Turner <liamturnerprop@gmail.com>
2022-01-30 09:12:45 +0000

    exercises 1-4 (the basics)

---
+++ the_basics/basics_ex3.rb
@@ -0,0 +1,11 @@
+movies = { the_godfather:  1972,
+           twelve_angry_men: 1957, 
+           casablanca: 1942, 
+           rear_window: 1954, 
+           city_lights: 1931 } 
+          
+puts movies[:the_godfather]
+puts movies[:twelve_angry_men]
+puts movies [:casablanca]
+puts movies [:rear_window]
+puts movies [:city_lights]
---
+++ the_basics/basics_ex4.rb
@@ -0,0 +1,7 @@
+dates = [1972, 1957, 1942, 1954, 1931]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
