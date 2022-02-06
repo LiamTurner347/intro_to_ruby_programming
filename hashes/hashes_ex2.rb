45edb30c0e7350e46ec55e4763de2f996f251ab7
Liam Turner <liamturnerprop@gmail.com>
2022-02-06 20:24:17 +0000

    Hashes - second batch of exercises

---
+++ hashes/hashes_ex2.rb
@@ -0,0 +1,10 @@
+a = {meat: "chicken", veg: "carrot", fruit: "strawberry"}
+a1 = {carbs: "pasta", dessert: "french toast"}
+
+p a.merge(a1)
+p a
+p a1
+
+p a.merge!(a1)
+p a
+p a1
---
+++ hashes/hashes_ex5.rb
@@ -0,0 +1,4 @@
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+
+p person.value?("Bob")
+p person.value?("Jeff")
